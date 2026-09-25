9a4a042e60736bfcfd8c5c8a7478edb11a4c72f1
dila <dilaalyarahma0403@gmail.com>
2026-09-25 16:23:34 +0700

    Selesai Langkah Praktikum Modul 4

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Kelas;
+use App\Models\UserModel;
+
+
+class UserController extends Controller
+{
+    public $userModel;
+    public $kelasModel;
+
+    public function __construct()
+    {
+
+    $this->userModel = new UserModel();
+    $this->kelasModel = new Kelas();
+}
+    public function create()
+    {
+        $kelasModel = new Kelas();
+        $kelas = $kelasModel->getKelas();
+
+        $data = [
+            'title' => 'Create User',
+            'kelas' => $kelas,
+        ];
+
+        return view('create_user', $data);
+    }
+
+    public function store(Request $request)
+{
+    $this->userModel->create([
+        'nama' => $request->input('nama'),
+        'npm' => $request->input('npm'),
+        'kelas_id' => $request->input('kelas_id'),
+    ]);
+
+    return redirect()->to('/user');
+}
+
+public function index()
+{
+    $users = $this->userModel->getUser();
+
+    $data = [
+        'title' => 'List User',
+        'users' => $users,
+    ];
+
+    return view('list_user', $data);
+}
+}
